e7321dcbc6afe7b4801d37e1461536330df7a30d
Matteo Ponzini <matteoponzini99@gmail.com>
2020-02-04 22:52:08 +0100

    Init commit

---
+++ src/main/java/it/matteoponzini/springpetclinc/SpringPetClincApplication.java
@@ -0,0 +1,13 @@
+package it.matteoponzini.springpetclinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClincApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClincApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/matteoponzini/springpetclinc/SpringPetClincApplicationTests.java
@@ -0,0 +1,13 @@
+package it.matteoponzini.springpetclinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClincApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
